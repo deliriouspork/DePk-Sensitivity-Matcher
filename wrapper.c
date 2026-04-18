f6f3edfb334c55ae71f26cdd024d4802f1f7922b
depk <wmajestic06@gmail.com>
2026-04-17 20:44:48 -0700

    god please

--- wrapper.c
+++ wrapper.c
@@ -1,10 +1,19 @@
 #include <unistd.h>
 #include <grp.h>
+#include <pwd.h>
 
 int main() {
-    gid_t gid = getegid();
-    setgid(gid);
-    setgroups(1, &gid);
+    // Get the input group GID
+    struct group *input_grp = getgrnam("input");
+    gid_t groups[] = { input_grp->gr_gid };
+
+    // Drop to input group only, keep real user
+    setgroups(1, groups);
+    setgid(input_grp->gr_gid);
+
+    // Drop root uid back to calling user
+    setuid(getuid());
+
     chdir("/usr/lib/depk-sensitivity-matcher");
     execl("/usr/bin/python", "python", "main.py", NULL);
     return 1;
