aba84553967fb6a61e75453f049975383320d5e0
depk <wmajestic06@gmail.com>
2026-04-17 20:14:29 -0700

    aur fix hopefully

--- wrapper.c
+++ wrapper.c
@@ -1,6 +1,10 @@
 #include <unistd.h>
+#include <grp.h>
 
 int main() {
+    gid_t gid = getegid();
+    setgid(gid);
+    setgroups(1, &gid);
     chdir("/usr/lib/depk-sensitivity-matcher");
     execl("/usr/bin/python", "python", "main.py", NULL);
     return 1;
